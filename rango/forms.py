2d426db2ba23a8e210cf4b0df69472fb4f8e9832
Kajetan Puchalski <kajetan.puchalski@tuta.io>
2021-02-07 21:23:14 +0000

    [Chapter 9] Sign up functionality

--- rango/forms.py
+++ rango/forms.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 from django import forms
-from rango.models import Page, Category
+from rango.models import Page, Category, UserProfile
+from django.contrib.auth.models import User
 
 
 class CategoryForm(forms.ModelForm):
@@ -45,3 +46,17 @@ class PageForm(forms.ModelForm):
             cleaned_data['url'] = url
 
         return cleaned_data
+
+
+class UserForm(forms.ModelForm):
+    password = forms.CharField(widget=forms.PasswordInput())
+
+    class Meta:
+        model = User
+        fields = ('username', 'email', 'password')
+
+
+class UserProfileForm(forms.ModelForm):
+    class Meta:
+        model = UserProfile
+        fields = ('website', 'picture')
